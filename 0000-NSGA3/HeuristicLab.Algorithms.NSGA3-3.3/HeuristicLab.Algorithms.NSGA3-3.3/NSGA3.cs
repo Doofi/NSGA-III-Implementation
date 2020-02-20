586992b909490944c4af866d26377ec6c89c916a
Dominik <dominik.leko3@gmail.com>
2020-02-20 16:22:53 +0100

    Create Dummy ReferencePoint and Solution classes. Add Parameters to NSGA3 class

--- 0000-NSGA3/HeuristicLab.Algorithms.NSGA3-3.3/HeuristicLab.Algorithms.NSGA3-3.3/NSGA3.cs
+++ 0000-NSGA3/HeuristicLab.Algorithms.NSGA3-3.3/HeuristicLab.Algorithms.NSGA3-3.3/NSGA3.cs
@@ -1,74 +1,174 @@
-﻿using HEAL.Attic;
-using HeuristicLab.Common; // required for parameters collection
-using HeuristicLab.Core; // required for parameters collection
-using HeuristicLab.Data; // IntValue, ...
-using HeuristicLab.Encodings.BinaryVectorEncoding;
-using HeuristicLab.Optimization; // BasicAlgorithm
-using HeuristicLab.Parameters;
-using HeuristicLab.Problems.Binary;
-using HeuristicLab.Random; // MersenneTwister
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading;
+using HEAL.Attic;
+using HeuristicLab.Common;
+using HeuristicLab.Core;
+using HeuristicLab.Data;
+using HeuristicLab.Encodings.RealVectorEncoding;
+using HeuristicLab.Optimization;
+using HeuristicLab.Parameters;
 
 namespace HeuristicLab.Algorithms.NSGA3
 {
-    // each HL item needs to have a name and a description (BasicAlgorithm is an Item) The name and
-    // description of items is shown in the GUI
+    /// <summary>
+    /// The Nondominated Sorting Genetic Algorithm III was introduced in Jain, Deb 2013: An
+    /// Evolutionary Many-Objective Optimization Algorithm Using Reference - Point - Based
+    /// Nondominated Sorting Approach, Part II: Handling Constraints and Extending to an Adaptive
+    /// Approach. IEEE Transactions on Evolutionary Computation (Volume: 18, Issue: 4, Aug, 2014)
+    /// pp. 602-622.
+    /// </summary>
     [Item("NSGA-III", "The Nondominated Sorting Genetic Algorithm III was introduced in Jain, Deb 2013: An Evolutionary Many-Objective Optimization Algorithm Using Reference - Point - Based Nondominated Sorting Approach, Part II: Handling Constraints and Extending to an Adaptive Approach. IEEE Transactions on Evolutionary Computation (Volume: 18, Issue: 4, Aug, 2014) pp. 602-622.")]
-
-    // If the algorithm should be shown in the "New..." dialog it must be creatable. Entries in the
-    // new dialog are grouped to categories and ordered by priorities
-    [Creatable(Category = CreatableAttribute.Categories.Algorithms, Priority = 999)]
-    [StorableType("689280F7-E371-44A2-98A5-FCEDF22CA343")] // for persistence (storing your algorithm to a files or transfer to HeuristicLab.Hive
-    public class NSGA3 : BasicAlgorithm
+    [Creatable(CreatableAttribute.Categories.PopulationBasedAlgorithms, Priority = 136)]
+    [StorableType("ce2961e8-0cd9-4dc8-b441-d9059a690874")]
+    internal class NSGA3 : BasicAlgorithm
     {
-        // createable items must have a default ctor
-        public NSGA3()
+        // Todo: get methods in order
+
+        public override bool SupportsPause => false; // todo: make true
+
+        #region ProblemProperties
+
+        public override Type ProblemType
         {
-            // algorithm parameters are shown in the GUI
-            Parameters.Add(new FixedValueParameter<IntValue>("MaxIterations", new IntValue(10000)));
+            get { return typeof(MultiObjectiveBasicProblem<RealVectorEncoding>); }
         }
 
-        // Persistence uses this ctor to improve deserialization efficiency. If we would use the
-        // default ctor instead this would completely initialize the object (e.g. creating
-        // parameters) even though the data is later overwritten by the stored data.
-        [StorableConstructor]
-        public NSGA3(StorableConstructorFlag _) : base(_) { }
-
-        // Each clonable item must have a cloning ctor (deep cloning, the cloner is used to handle
-        // cyclic object references)
-        public NSGA3(NSGA3 original, Cloner cloner) : base(original, cloner)
+        public new MultiObjectiveBasicProblem<RealVectorEncoding> Problem
         {
-            // Don't forget to call the cloning ctor of the base class This class does not have
-            // fields, therefore we don't need to actually clone anything
+            get { return (MultiObjectiveBasicProblem<RealVectorEncoding>)base.Problem; }
+            set { base.Problem = value; }
         }
 
-        // This algorithm only works for BinaryProblems. Overriding the ProblemType property has the
-        // effect that only BinaryProblems can be set as problem for the algorithm in the GUI
-        public override Type ProblemType { get { return typeof(BinaryProblem); } }
+        #endregion ProblemProperties
 
-        public new BinaryProblem Problem { get { return (BinaryProblem)base.Problem; } }
+        #region Storable fields
 
-        #region parameters
+        #endregion Storable fields
 
-        public int MaxIterations
+        #region ParameterNames
+
+        #endregion ParameterNames
+
+        #region ParameterProperties
+
+        private ValueParameter<IntValue> SeedParameter
         {
-            get { return MaxIterationsParameter.Value.Value; }
-            set { MaxIterationsParameter.Value.Value = value; }
+            get { return (ValueParameter<IntValue>)Parameters["Seed"]; }
         }
 
-        // If an algorithm has parameters then we usually also add properties to access these
-        // parameters. This is not strictly required but considered good shape.
-        private IFixedValueParameter<IntValue> MaxIterationsParameter
+        private ValueParameter<BoolValue> SetSeedRandomlyParameter
         {
-            get { return (IFixedValueParameter<IntValue>)Parameters["MaxIterations"]; }
+            get { return (ValueParameter<BoolValue>)Parameters["SetSeedRandomly"]; }
         }
 
-        #endregion parameters
-
-        public override bool SupportsPause
+        private ValueParameter<IntValue> PopulationSizeParameter
         {
-            get { return false; }
+            get { return (ValueParameter<IntValue>)Parameters["PopulationSize"]; }
+        }
+
+        private ValueParameter<PercentValue> CrossoverProbabilityParameter
+        {
+            get { return (ValueParameter<PercentValue>)Parameters["CrossoverProbability"]; }
+        }
+
+        private ValueParameter<PercentValue> MutationProbabilityParameter
+        {
+            get { return (ValueParameter<PercentValue>)Parameters["MutationProbability"]; }
+        }
+
+        //private ValueParameter<MultiAnalyzer> AnalyzerParameter
+        //{
+        //    get { return (ValueParameter<MultiAnalyzer>)Parameters["Analyzer"]; }
+        //}
+
+        private ValueParameter<IntValue> MaximumGenerationsParameter
+        {
+            get { return (ValueParameter<IntValue>)Parameters["MaximumGenerations"]; }
+        }
+
+        private IFixedValueParameter<BoolValue> DominateOnEqualQualitiesParameter
+        {
+            get { return (IFixedValueParameter<BoolValue>)Parameters["DominateOnEqualQualities"]; }
+        }
+
+        #endregion ParameterProperties
+
+        #region Properties
+
+        public IntValue Seed
+        {
+            get { return SeedParameter.Value; }
+            set { SeedParameter.Value = value; }
+        }
+
+        public BoolValue SetSeedRandomly
+        {
+            get { return SetSeedRandomlyParameter.Value; }
+            set { SetSeedRandomlyParameter.Value = value; }
+        }
+
+        public IntValue PopulationSize
+        {
+            get { return PopulationSizeParameter.Value; }
+            set { PopulationSizeParameter.Value = value; }
+        }
+
+        public PercentValue CrossoverProbability
+        {
+            get { return CrossoverProbabilityParameter.Value; }
+            set { CrossoverProbabilityParameter.Value = value; }
+        }
+
+        public PercentValue MutationProbability
+        {
+            get { return MutationProbabilityParameter.Value; }
+            set { MutationProbabilityParameter.Value = value; }
+        }
+
+        //public MultiAnalyzer Analyzer
+        //{
+        //    get { return AnalyzerParameter.Value; }
+        //    set { AnalyzerParameter.Value = value; }
+        //}
+
+        public IntValue MaximumGenerations
+        {
+            get { return MaximumGenerationsParameter.Value; }
+            set { MaximumGenerationsParameter.Value = value; }
+        }
+
+        public bool DominateOnEqualQualities
+        {
+            get { return DominateOnEqualQualitiesParameter.Value.Value; }
+            set { DominateOnEqualQualitiesParameter.Value.Value = value; }
+        }
+
+        #endregion Properties
+
+        #region ResultsProperties
+
+        #endregion ResultsProperties
+
+        #region Constructors
+
+        // Called when creating new NSGA3 instance
+        public NSGA3() : base()
+        {
+            Parameters.Add(new ValueParameter<IntValue>("Seed", "The random seed used to initialize the new pseudo random number generator.", new IntValue(0)));
+            Parameters.Add(new ValueParameter<BoolValue>("SetSeedRandomly", "True if the random seed should be set to a random value, otherwise false.", new BoolValue(true)));
+            Parameters.Add(new ValueParameter<IntValue>("PopulationSize", "The size of the population of solutions.", new IntValue(100)));
+            Parameters.Add(new ValueParameter<PercentValue>("CrossoverProbability", "The probability that the crossover operator is applied on two parents.", new PercentValue(0.9)));
+            Parameters.Add(new ValueParameter<PercentValue>("MutationProbability", "The probability that the mutation operator is applied on a solution.", new PercentValue(0.05)));
+            //Parameters.Add(new ValueParameter<MultiAnalyzer>("Analyzer", "The operator used to analyze each generation.", new MultiAnalyzer()));
+            Parameters.Add(new ValueParameter<IntValue>("MaximumGenerations", "The maximum number of generations which should be processed.", new IntValue(1000)));
+            Parameters.Add(new FixedValueParameter<BoolValue>("DominateOnEqualQualities", "Flag which determines wether solutions with equal quality values should be treated as dominated.", new BoolValue(false)));
+        }
+
+        protected NSGA3(NSGA3 original, Cloner cloner) : base(original, cloner)
+        {
+            // Todo: clone Storable fields
         }
 
         public override IDeepCloneable Clone(Cloner cloner)
@@ -76,40 +176,73 @@ namespace HeuristicLab.Algorithms.NSGA3
             return new NSGA3(this, cloner);
         }
 
+        #endregion Constructors
+
+        #region Initialization
+
+        #endregion Initialization
+
         protected override void Run(CancellationToken cancellationToken)
         {
-            int maxIters = MaxIterations;
-            var problem = Problem;
-            var rand = new MersenneTwister(1234);
-
-            var bestQuality = problem.Maximization ? double.MinValue : double.MaxValue;
-
-            var curItersItem = new IntValue();
-            var bestQualityItem = new DoubleValue(bestQuality);
-            var curItersResult = new Result("Iteration", curItersItem);
-            var bestQualityResult = new Result("Best quality", bestQualityItem);
-            Results.Add(curItersResult);
-            Results.Add(bestQualityResult);
-
-            for (int i = 0; i < maxIters; i++)
+            while (true)
             {
-                curItersItem.Value = i;
+                Debug.WriteLine("Hello World! from Run");
 
-                // ----------------------------- IMPLEMENT YOUR ALGORITHM HERE -----------------------------
-
-                // this is an example for random search for a more elaborate algorithm check the
-                // source code of "HeuristicLab.Algorithms.ParameterlessPopulationPyramid"
-                var cand = new BinaryVector(problem.Length, rand);
-                var quality = problem.Evaluate(cand, rand); // calling Evaluate like this is not possible for all problems...
-                if (problem.Maximization) bestQuality = Math.Max(bestQuality, quality);
-                else bestQuality = Math.Min(quality, bestQuality);
-                bestQualityItem.Value = bestQuality;
-
-                // check the cancellation token to see if the used clicked "Stop"
-                if (cancellationToken.IsCancellationRequested) break;
+                Thread.Sleep(100);
             }
+        }
 
-            Results.Add(new Result("Execution time", new TimeSpanValue(this.ExecutionTime)));
+        private const int dimensionCount = 10; // todo: use parameters
+
+        /// <summary>
+        /// returns population of next generation after applying necessary operations
+        /// </summary>
+        /// <param name="pt">parent population</param>
+        /// <param name="referencePoints">structured reference points or supplied aspiration points</param>
+        /// <returns></returns>
+        private List<Solution> GetNextGeneration(List<Solution> pt, List<ReferencePoint> referencePoints)
+        {
+            bool aspirationPointsWereSupplied = false; // todo: use parameters
+
+            List<Solution> st = new List<Solution>();
+            int i = 1;
+
+            Recombine(pt);
+            Mutate(pt);
+            double[][] qualities = Evaluate(pt);
+            bool[] maximization = new bool[dimensionCount];
+            int[] rank;
+
+            List<List<Tuple<Solution, double[]>>> allParetoFronts = DominationCalculator<Solution>.CalculateAllParetoFronts(pt.ToArray(), qualities, maximization, out rank, true);
+
+            return null;
+        }
+
+        private void Recombine(List<Solution> pt)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Mutate(List<Solution> pt)
+        {
+            throw new NotImplementedException();
+        }
+
+        private double[][] Evaluate(List<Solution> pt)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Called when Algorithm is starting to execute. todo: remove this comment
+        protected override void Initialize(CancellationToken cancellationToken)
+        {
+            int paretoSolutionCount = 20; // todo: remove
+
+            // Todo: Initialize results screen
+            Results.Add(new Result("Iterations", new IntValue(0)));
+            Results.Add(new Result("Best solutions", new IntMatrix(dimensionCount, paretoSolutionCount)));
+
+            base.Initialize(cancellationToken);
         }
     }
 }
